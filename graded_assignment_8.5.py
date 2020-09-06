49fc4f8d3e4492bf04e4b7ae26dc2fd8922ddb5b
Rajasudhangowda321 <69045108+Rajasudhangowda321@users.noreply.github.com>
2020-09-06 21:03:42 +0530

    Update graded_assignment_8.5.py

--- graded_assignment_8.5.py
+++ graded_assignment_8.5.py
@@ -2,14 +2,14 @@
 #From stephen.marquard@uct.ac.za Sat Jan  5 09:14:16 2008
 #You will parse the From line using split() and print out the second word in the line (i.e. the entire address of the person who sent the message). Then print out a count at the end.
 #Hint: make sure not to include the lines that start with 'From:'.
-fname = input("Enter file name: ")
-fh = open(fname)
+file_name = input("Enter file name: ")
+file_handle = open(file_name)
 count = 0
-for line in fh:
+for line in file_handle:
     line=line.strip()
     if line.startswith('From '):
         count+=1
-        li=line.split()
-        word=li[1]
+        x=line.split()
+        word=x[1]
         print(word)
 print("There were", count, "lines in the file with From as the first word")
