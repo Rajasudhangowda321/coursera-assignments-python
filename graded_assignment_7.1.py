d77f09ff44743ba877da33c573cb1dd1ea431a2d
Rajasudhangowda321 <69045108+Rajasudhangowda321@users.noreply.github.com>
2020-09-06 20:47:34 +0530

    Update graded_assignment_7.1.py

--- graded_assignment_7.1.py
+++ graded_assignment_7.1.py
@@ -1,5 +1,5 @@
 #Write a program that prompts for a file name, then opens that file and reads through the file, and print the contents of the file in upper case. Use the file words.txt to produce the output below.
-fname=input("enter the file name:")
-o=open(fname)
-r=o.read().strip()
-print(r.upper())
+file_name=input("enter the file name:")
+r=open(file_name)
+x=r.read().strip()
+print(x.upper())
