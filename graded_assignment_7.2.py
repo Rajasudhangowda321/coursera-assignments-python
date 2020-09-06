3cd6370fa54af5b1b01cee8210859a3999ce26bc
Rajasudhangowda321 <69045108+Rajasudhangowda321@users.noreply.github.com>
2020-09-06 20:48:35 +0530

    Update graded_assignment_7.2.py

--- graded_assignment_7.2.py
+++ graded_assignment_7.2.py
@@ -2,15 +2,15 @@
 #X-DSPAM-Confidence:    0.8475
 #Count these lines and extract the floating point values from each of the lines and compute the average of those values and produce an output as shown below. Do not use the sum() function or a variable named sum in your solution.
 # Use the file name mbox-short.txt as the file name
-fname = input("Enter file name: ")
-fh = open(fname)
+file_name = input("Enter file name: ")
+file_handle= open(file_name)
 count=0
 total=0
-for line in fh:
+for line in file_handle:
     if not line.startswith("X-DSPAM-Confidence: ") :
         continue
     count+=1
     count=float(count)
-    r=line[20:]
-    total=float(r)+float(total)
+    x=line[20:]
+    total=float(x)+float(total)
 print("Average spam confidence:",total/count)
