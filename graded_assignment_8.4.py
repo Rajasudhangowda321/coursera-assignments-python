b07b4b80ff065b9907de5fe967c73b5980fd295e
Rajasudhangowda321 <69045108+Rajasudhangowda321@users.noreply.github.com>
2020-09-06 21:00:24 +0530

    Update graded_assignment_8.4.py

--- graded_assignment_8.4.py
+++ graded_assignment_8.4.py
@@ -1,12 +1,12 @@
 # Open the file romeo.txt and read it line by line. For each line, split the line into a list of words using the split() method. The program should build a list of words. For each word on each line check to see if the word is already in the list and if not append it to the list. When the program completes, sort and print the resulting words in alphabetical order.
-fname=input("Enter file name:")
-fh=open(fname)
+file_name=input("Enter file name:")
+file_handle=open(file_name)
 lst=list()
-for line in fh:
-    line=line.rstrip()
-    line=line.split()
+for line in file_handle:
+    line=line.rstrip()#it removes the space in the line
+    line=line.split()#split takes the string and gives list
     for words in line:
-        if not words in lst:
+        if not words in lst:#if the word is not in the list it adds it.
             lst.append(words)
 lst.sort()
 print(lst)
